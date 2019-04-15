f507487329405613cec047853f4500b5205d783a
Rashad Attaf <rashadattaf@gmail.com>
2019-04-15 11:57:50 +0300

    feat: create tow functions to choose cards randomly
    pickCards(array) : is a function to choose 8 cards randomly from an
    array that will be passed to the function as a parameter
    and after choosing the cards they will be doubled
    because we have 16 cards.
    the 16 chosen cards will be returned as an array called : pickedArr.
    
    shuffle(array) : is a function to shuffle a given array and return it.
    so after we create the pickedArr we will pass it to the shuffle()
    so the cards will be randomly shuffled.

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ let cards = ['\u2702', '\u2708', '\u270E', '\u2719', '\u2729', '\u273E',
   '\u2660', '\u2663', '\u2665', '\u2666', '\u2668', '\u2672'
   , '\u26C0', '\u26C1'];
 
-//   playingCards array will handle the cards while the game is on playing.
+// playingCards array will handle the cards while the game is on playing.
 let playingCards = [];
 // the mvs variable will keep the value of moves player have made
 let mvs = 0;
@@ -19,3 +19,40 @@ let minutes = 0;
 let seconds = 0;
 // the sep is just ':' symbol for seperate between minutes and seconds
 let sep = ':';
+
+// this function to pick 8 cards randomly from a given array
+// and double them to 16 cards and put them in an array
+// and return it . 
+function pickCards(array) {
+  let startArr = array;
+  let pickedArr = [];
+  for (let i = 0; i < 8; i++) {
+    let ranNum = (Math.floor(Math.random() * startArr.length));
+    pickedArr.push(startArr[ranNum]);
+    pickedArr.push(startArr[ranNum]);
+    startArr.splice(ranNum, 1);
+  }
+  return pickedArr;
+}
+
+// to shuffle a given array
+// this function was taken from
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array/2450976#2450976
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
